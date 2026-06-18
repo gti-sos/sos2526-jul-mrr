d2c4f939cc08c1183f6166b06b79ab382bc41ca8
mariarguezro <mariarguezro@gmail.com>
2026-06-18 12:58:41 +0200

    Creación entorno svelte

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { loadBackend } from './src/back/index.js';
-
+import cors from 'cors';
+import { handler } from './src/front/build/handler.js';
 
 let PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
+
+//app.use('/', express.static('public'));
 app.use(bodyParser.json());
-app.use('/', express.static('public'));
 
 loadBackend(app);
+
+app.use(handler);
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
